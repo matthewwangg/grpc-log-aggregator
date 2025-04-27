151dc78588b6b2ef209088bc242974a85190cd2e
Matt Wang <matthewianwang@gmail.com>
2025-04-26 19:52:22 -0700

    build(cmake): add initial CMakeLists.txt and build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+echo "[-] Cleaning old generated proto files..."
+rm -f proto/*.pb.cc proto/*.pb.h proto/*.grpc.pb.cc proto/*.grpc.pb.h
+
+echo "[+] Regenerating proto and gRPC files..."
+protoc --proto_path=proto --cpp_out=proto proto/log.proto
+protoc --proto_path=proto --grpc_out=proto --plugin=protoc-gen-grpc=/usr/bin/grpc_cpp_plugin proto/log.proto
+
+echo "[+] Setting up build directory..."
+rm -rf build
+mkdir build
+cd build
+
+echo "[+] Running CMake..."
+cmake ..
+
+echo "[+] Building project..."
+make -j$(nproc)
+
+echo "[✓] Build complete."
