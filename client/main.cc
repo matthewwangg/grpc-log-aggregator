15ef7df62162fc07689007350cfaaa7cf6c1c38d
Matt Wang <matthewianwang@gmail.com>
2025-04-29 23:33:47 -0700

    feat(client): add test for StreamLog to client

--- client/main.cc
+++ client/main.cc
@@ -7,14 +7,7 @@
 #include "log.grpc.pb.h"
 #include "log.pb.h"
 
-int main(int argc, char* argv[]) {
-    std::string server_address = "127.0.0.1:50051";
-    std::shared_ptr<grpc::Channel> channel = grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials());
-
-    std::unique_ptr<log::LogService::Stub> stub = log::LogService::NewStub(channel);
-
-    grpc::ClientContext context;
-
+void ExampleSendLog(std::shared_ptr<log::LogService::Stub> stub, std::unique_ptr<grpc::ClientContext> context) {
     google::protobuf::Timestamp ts;
     auto now = std::chrono::system_clock::now();
     ts.set_seconds(std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count());
@@ -28,12 +21,58 @@ int main(int argc, char* argv[]) {
 
     log::LogResponse response;
 
-    grpc::Status status = stub->SendLog(&context, request, &response);
+    grpc::Status status = stub->SendLog(context.get(), request, &response);
 
     if (status.ok()) {
-        std::cout << "RPC succeeded!" << std::endl;
+        std::cout << "SendLog succeeded!" << std::endl;
     } else {
-        std::cout << "RPC failed with error code: " << status.error_code()
+        std::cout << "SendLog failed with error code: " << status.error_code()
                   << ", message: " << status.error_message() << std::endl;
     }
+}
+
+void ExampleStreamLog(std::shared_ptr<log::LogService::Stub> stub, std::unique_ptr<grpc::ClientContext> context) {
+    google::protobuf::Timestamp ts;
+
+    log::LogEntry request;
+    request.set_level("INFO");
+    request.set_source("client-test");
+    request.set_hostname("127.0.0.1");
+
+    log::LogResponse response;
+
+    std::unique_ptr<grpc::ClientWriter<log::LogEntry>> writer = stub->StreamLog(context.get(), &response);
+
+    for (int i = 0; i < 5; i++) {
+        auto now = std::chrono::system_clock::now();
+        ts.set_seconds(std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count());
+
+        *request.mutable_timestamp() = ts;
+        request.set_message("Stream test " + std::to_string(i));
+
+        writer->Write(request);
+    }
+    writer->WritesDone();
+
+    grpc::Status status = writer->Finish();
+
+    if (status.ok()) {
+        std::cout << "StreamLog succeeded!" << std::endl;
+    } else {
+        std::cout << "StreamLog failed with error code: " << status.error_code()
+                  << ", message: " << status.error_message() << std::endl;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    std::string server_address = "127.0.0.1:50051";
+    std::shared_ptr<grpc::Channel> channel = grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials());
+
+    std::shared_ptr<log::LogService::Stub> stub = log::LogService::NewStub(channel);
+
+    std::unique_ptr<grpc::ClientContext> send_log_context = std::make_unique<grpc::ClientContext>();
+    ExampleSendLog(stub, std::move(send_log_context));
+
+    std::unique_ptr<grpc::ClientContext> stream_log_context = std::make_unique<grpc::ClientContext>();
+    ExampleStreamLog(stub, std::move(stream_log_context));
 }
