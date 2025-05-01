b122d3992b85f3cc351695d432a1e44b119243c7
Matt Wang <matthewianwang@gmail.com>
2025-05-01 00:51:48 -0700

    feat(log): add log file reading support

--- include/utils/log_utils.h
+++ include/utils/log_utils.h
@@ -1,12 +1,17 @@
 #ifndef GRPC_LOG_AGGREGATOR_LOG_UTILS_H
 #define GRPC_LOG_AGGREGATOR_LOG_UTILS_H
 
+#include <filesystem>
+#include <vector>
+
 #include <grpcpp/grpcpp.h>
 
 #include "log.pb.h"
 
 namespace log_utils {
 
+bool ParseLogLine(const std::string& line, log::LogEntry& entry, const std::string& date);
+grpc::Status ReadLogFileToEntries(const std::filesystem::path& file_path, const std::string& keyword, const std::string& source_filter, std::vector<log::LogEntry>& entries);
 grpc::Status WriteLogEntryToFile(const log::LogEntry& entry);
 
 } // namespace log_utils
